7b783f21bd8cdf689e1636bf6aea14d7fd5c1898
Scott <patentclerks@gmail.com>
2019-06-06 14:51:36 -0400

    Vehicle Interface and Vehicle Classes

---
+++ src/TTS/TeamBlue/Dealership/Classes/Suv.java
@@ -0,0 +1,35 @@
+package TTS.TeamBlue.Dealership.Classes;
+
+import TTS.TeamBlue.Dealership.Interfaces.Vehicle;
+
+public class Suv implements Vehicle {
+	
+	private String make;
+	private String model;
+	private int year;
+	
+	public Suv(String make, String model, int year) {
+		this.make = make;
+		this.model = model;
+		this.year = year;
+	}
+
+	@Override
+	public String getMake() {
+		
+		return this.make;
+	}
+
+	@Override
+	public String getModel() {
+		
+		return this.model;
+	}
+
+	@Override
+	public int getYear() {
+		
+		return this.year;
+	}
+
+}
---
+++ src/TTS/TeamBlue/Dealership/Interfaces/Vehicle.java
@@ -0,0 +1,9 @@
+package TTS.TeamBlue.Dealership.Interfaces;
+
+public interface Vehicle {
+	
+	public String getMake();
+	public String getModel();
+	public int getYear();
+	
+}
