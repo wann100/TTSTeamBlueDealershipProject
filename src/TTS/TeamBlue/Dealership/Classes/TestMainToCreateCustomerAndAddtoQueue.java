db0b9106595240c958ed747c02778db68e2df1de
Gregory <gregory.wyche@protonmail.com>
2019-06-11 11:58:48 -0400

    Carry out user onboarding, providing a loaded queue to the vehicle and the payment method sub departments. contains queues for payment method sub departments, updates them, and calculates their queue length

--- src/TTS/TeamBlue/Dealership/Classes/TestMainToCreateCustomerAndAddtoQueue.java
+++ src/TTS/TeamBlue/Dealership/Classes/TestMainToCreateCustomerAndAddtoQueue.java
@@ -1,6 +1,10 @@
 package TTS.TeamBlue.Dealership.Classes;
 import java.util.*;
 
+import TTS.TeamBlue.Dealership.Utility.QueueIndexTool;
+import TTS.TeamBlue.Dealership.Utility.QueueIndexTool2;
+
+
 public class TestMainToCreateCustomerAndAddtoQueue {
 
 	public static void main(String[] args) {
@@ -9,17 +13,18 @@ public class TestMainToCreateCustomerAndAddtoQueue {
 		Scanner scanner = new Scanner(System.in);
 		
 		//Creates a blank Queue
-		Queue<CustomerQueueItem> mainQueue = new LinkedList<CustomerQueueItem>(); 
+		Queue<CustomerQueueItem> mainQueueInitial = new LinkedList<CustomerQueueItem>(); 
 		
-		//Create a new customer instance and retrieve the modified Queue this will generate
-		mainQueue = UserOnboarding.userOnboarding();
+		//Create a new customer instance, add it to the existing Queue and retrieve that modified Queue
+		mainQueueInitial = UserOnboarding.userOnboarding();
 		
-		//REMOVE ALL USERS AHEAD OF THE CURRENT USER IN THE QUEUE
-		
+		//REMOVE ALL USERS AHEAD OF THE CURRENT USER IN THE QUEUE 
+		//This is done by passing the Queue to a utility method that returns a new Queue with only the last user added, the one we on-boarded
+		Queue mainQueue = QueueIndexTool.QueueIndexTool(mainQueueInitial, 2);
 		//
 		
-		//Retrieve and delete the first name inside the Queue item at the top of the Queue
-		String testretrievefName = ((mainQueue.peek()).getGuestinfo()).getFirstName();
+		//Retrieve the first name inside the Queue item at the top of the Queue
+		String testretrievefName = (((CustomerQueueItem) mainQueue.peek()).getGuestinfo()).getFirstName();
 		System.out.println("Thank you for that information, "+testretrievefName);
 		System.out.println("Are you interested purchasing in full today or do you plan to lease or finance after you choose and customize you vehicle.");
 		System.out.println("You can type 'full' to buy outright, 'lease' to speak to a leasing representative later or 'finace' to speak to a financing representative.");
@@ -29,47 +34,57 @@ public class TestMainToCreateCustomerAndAddtoQueue {
 		int deptchoicenumber = 0;
 		if (departmentchoice.equalsIgnoreCase("full")){
 			deptchoicenumber = 1;
-			System.out.println("No problem, "+testretrievefName+"We'll transfer you to the payment department after you choose your vehicle.");
+			System.out.println("No problem, "+testretrievefName+" We'll transfer you to the payment department after you choose your vehicle.");
 		}
 		if (departmentchoice.equalsIgnoreCase("lease")){
 			deptchoicenumber = 2;
-			System.out.println("No problem, "+testretrievefName+"We'll transfer you to the leasing department after you choose your vehicle.");
+			System.out.println("No problem, "+testretrievefName+" We'll transfer you to the leasing department after you choose your vehicle.");
 		}
 		if (departmentchoice.equalsIgnoreCase("finance")){
 			deptchoicenumber = 3;
-			System.out.println("No problem, "+testretrievefName+"We'll transfer you to the financing department after you choose your vehicle.");
+			System.out.println("No problem, "+testretrievefName+" We'll transfer you to the financing department after you choose your vehicle.");
 		}
 		
 		
-		//Print out the current Queue
-		testretrievefName = ((mainQueue.poll()).getGuestinfo()).getFirstName();
-		System.out.println(testretrievefName);
+		
+		
+		
 		
 		
 		
 		//THIS WILL COME MUCH LATER, AFTER SCOTT PROBABLY
 		//TRANSFER USER TO CORRECT QUEUE WHEN THEY ARE READY TO BUY
-		Queue<CustomerQueueItem> purchasingQueue = new LinkedList<CustomerQueueItem>(); 
-		Queue<CustomerQueueItem> leasingQueue = new LinkedList<CustomerQueueItem>(); 
-		Queue<CustomerQueueItem> financingQueue = new LinkedList<CustomerQueueItem>(); 
+		//PRINT OUT THE LENGTH OF EACH DEPARTMENT'S QUEUE
+		Queue<CustomerQueueItem> PurchasingQueue = new LinkedList<CustomerQueueItem>(); 
+		Queue<CustomerQueueItem> LeasingQueue = new LinkedList<CustomerQueueItem>(); 
+		Queue<CustomerQueueItem> FinancingQueue = new LinkedList<CustomerQueueItem>(); 
 		
 		if(deptchoicenumber == 1) {
 			//Assign to purchasingQueue
 			System.out.println("Okay, "+testretrievefName+" We're transfering you to the purchasing department now!");
-			purchasingQueue = mainQueue;
+			PurchasingQueue = mainQueue;
+			int PurchasingQueueLength = QueueIndexTool2.QueueIndexTool2(PurchasingQueue);
+			System.out.println("Purchasing Queue Length: "+PurchasingQueueLength);
 		}
 		if(deptchoicenumber == 2) {
 			//Assign to leasingQueue
 			System.out.println("Okay, "+testretrievefName+" We're transfering you to the leasing department now!");
-			leasingQueue = mainQueue;
+			LeasingQueue = mainQueue;
+			int LeasingQueueLength = QueueIndexTool2.QueueIndexTool2(LeasingQueue);
+			System.out.println("Leasing Queue Length: "+LeasingQueueLength);
 		}
 		if(deptchoicenumber == 3) {
 			//Assign to financingQueue
 			System.out.println("Okay, "+testretrievefName+" We're transfering you to the financing department now!");
-			financingQueue = mainQueue;
+			FinancingQueue = mainQueue;
+			int FinancingQueueLength = QueueIndexTool2.QueueIndexTool2(FinancingQueue);
+			System.out.println("Finance Queue Length: "+FinancingQueueLength);
 		}
 		
-
+		
+		//Remove the QueueItem for the customer at the head of mainQueue. The only record of that customer now is in one of the department Queues.
+		CustomerQueueItem removedQueueHead = ((CustomerQueueItem) mainQueue.remove());
+		System.out.println(testretrievefName);
 
 	}
 
