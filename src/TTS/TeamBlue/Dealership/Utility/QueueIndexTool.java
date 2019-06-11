994c696ea42b762d91c794ed91d3e74a83d3161a
Gregory <gregory.wyche@protonmail.com>
2019-06-11 10:23:36 -0400

    utility to print out all first names in a queue and return the last queueitem added to the queue

---
+++ src/TTS/TeamBlue/Dealership/Utility/QueueIndexTool.java
@@ -0,0 +1,40 @@
+package TTS.TeamBlue.Dealership.Utility;
+import java.util.*;
+import TTS.TeamBlue.Dealership.Classes.CustomerQueueItem;
+
+public class QueueIndexTool {
+	
+	/*This utility recieves a Queue and an int.
+	If the int mode is set to 1, the method prints, the name of every client in the Queue
+	If the int mode is set to 2, the method returns the currentcustomer's QueueItem, ignoring the other Queue items ahead of it in line
+	*/
+	public static Queue QueueIndexTool(Queue mainQueue, int mode) {
+	
+	//Create black Queue to return later
+	Queue<CustomerQueueItem> newQueue = new LinkedList<CustomerQueueItem>(); 
+	
+	//Convert passed Queue to an array
+	Object QueueArray[] = mainQueue.toArray();
+	//Measure its length
+	int length = QueueArray.length;
+	
+	//Create a new Queue item
+	CustomerQueueItem itemtoreturn = new CustomerQueueItem();
+	
+	//Mode 1: print all the first names in the queue
+	if(mode == 1) {
+	for(int i = 0; i < QueueArray.length; i++) {
+	System.out.println((((CustomerQueueItem)(QueueArray[i])).getGuestinfo()).getFirstName());
+	}
+	}
+	
+	//Mode 2: extract the most recently added queue item in the passed queue, add it to the new queue you created and return that new queue
+	if(mode == 2) {
+		itemtoreturn = (CustomerQueueItem)QueueArray[QueueArray.length - 1];
+	}
+	
+	newQueue.add(itemtoreturn);
+	return newQueue;
+	
+	}
+}
