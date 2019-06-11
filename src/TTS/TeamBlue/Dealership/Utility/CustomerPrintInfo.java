134d1a33f45e1c422a1a29e4314d4e962ab1d563
Gregory <gregory.wyche@protonmail.com>
2019-06-11 14:18:20 -0400

    Utility prints the first name of the customer object in the customerqueueitem at the head of the queue

--- src/TTS/TeamBlue/Dealership/Utility/CustomerPrintInfo.java
+++ src/TTS/TeamBlue/Dealership/Utility/CustomerPrintInfo.java
@@ -1,7 +1,13 @@
 package TTS.TeamBlue.Dealership.Utility;
+import java.util.*;
+import TTS.TeamBlue.Dealership.Classes.CustomerQueueItem;
 
 public class CustomerPrintInfo {
 	
-	//public static void CustomerPrintInfo
+	public static void CustomerPrintInfo(CustomerQueueItem item) {
+		
+		
+		
+	}
 
 }
---
+++ src/TTS/TeamBlue/Dealership/Utility/FirstNameInQueue.java
@@ -0,0 +1,16 @@
+package TTS.TeamBlue.Dealership.Utility;
+import java.util.*;
+import TTS.TeamBlue.Dealership.Classes.CustomerQueueItem;
+
+public class FirstNameInQueue {
+	
+	//Utility returns the first name of first user in the Queue
+	public static String FirstNameInQueue(Queue input) {
+		String firstName;
+		
+		Object QueueArray [] = (input.toArray());
+		String customername = (((CustomerQueueItem)QueueArray[0]).getGuestinfo()).getFirstName();
+		return customername;
+	}
+
+}
