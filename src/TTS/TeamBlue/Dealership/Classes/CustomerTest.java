ca8115a3687c0f36be20585c2e54c2be4840ada9
Gregory <gregory.wyche@protonmail.com>
2019-06-13 15:06:35 -0400

    JUnit Test 1

--- src/TTS/TeamBlue/Dealership/Classes/CustomerTest.java
+++ src/TTS/TeamBlue/Dealership/Classes/CustomerTest.java
@@ -1,14 +1,17 @@
 package TTS.TeamBlue.Dealership.Classes;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import java.util.*;
+
+
 
 class CustomerTest {
 
 	@Test
-	void testGetFirstName() {
-		fail("Not yet implemented");
+	public void testGetFirstName() {
+		Customer testCustomer = new Customer();
+		String result = testCustomer.getFirstName();
+		assertEquals("default", result);
 	}
 
 }
