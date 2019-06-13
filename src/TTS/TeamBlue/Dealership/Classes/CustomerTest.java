aa7b422a78b04c10af092a2e47029b62f9c17d99
Gregory <gregory.wyche@protonmail.com>
2019-06-13 14:14:41 -0400

    Create test file for Customer. Unfinished right now.

---
+++ src/TTS/TeamBlue/Dealership/Classes/CustomerTest.java
@@ -0,0 +1,14 @@
+package TTS.TeamBlue.Dealership.Classes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerTest {
+
+	@Test
+	void testGetFirstName() {
+		fail("Not yet implemented");
+	}
+
+}
