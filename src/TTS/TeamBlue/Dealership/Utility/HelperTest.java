59eb6168042ccbf81c66e4706c9e5faf4383a550
Gregory <gregory.wyche@protonmail.com>
2019-06-14 12:50:09 -0400

    JUnit Test 3 complete

--- src/TTS/TeamBlue/Dealership/Utility/HelperTest.java
+++ src/TTS/TeamBlue/Dealership/Utility/HelperTest.java
@@ -1,14 +1,28 @@
 package TTS.TeamBlue.Dealership.Utility;
-
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.LinkedList;
+import java.util.Queue;
 import org.junit.jupiter.api.Test;
+import TTS.TeamBlue.Dealership.Customer.CustomerQueueItem;
+import TTS.TeamBlue.Dealership.Customer.Fake.CustomerAbe;
+import TTS.TeamBlue.Dealership.Customer.Fake.CustomerBob;
 
 class HelperTest {
 
 	@Test
 	void testQueueIndexTool() {
-		fail("Not yet implemented");
+		Queue<CustomerQueueItem> testQueue = new LinkedList<CustomerQueueItem>();
+
+		CustomerQueueItem testItem1 = new CustomerQueueItem();
+		CustomerQueueItem testItem2 = new CustomerQueueItem();
+		CustomerQueueItem testItem3 = new CustomerQueueItem();
+		testQueue.add(testItem1);
+		testQueue.add(testItem2);
+		testQueue.add(testItem3);
+		
+		int measurelength = Helper.QueueIndexTool2(testQueue);
+		assertEquals(3, measurelength);
+		
 	}
 
 }
