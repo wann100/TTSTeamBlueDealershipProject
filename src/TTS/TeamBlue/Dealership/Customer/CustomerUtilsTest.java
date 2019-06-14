32613f4cff6f6eac5b25dfa76b187e29c3dbfb33
Gregory <gregory.wyche@protonmail.com>
2019-06-14 12:27:04 -0400

    JUnit test 2.

---
+++ src/TTS/TeamBlue/Dealership/Customer/CustomerUtilsTest.java
@@ -0,0 +1,15 @@
+package TTS.TeamBlue.Dealership.Customer;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CustomerUtilsTest extends CustomerUtils {
+
+	@Test
+	void testCheckPastCustomerDatabase() {
+		String teststring = "rogerbannon@i9.gov";
+		String testCustomerfName = CheckPastCustomerDatabase(teststring).getFirstName();
+		assertEquals("Roger", testCustomerfName);
+		
+	}
+
+}
