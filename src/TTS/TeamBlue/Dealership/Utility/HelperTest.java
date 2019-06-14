9596b0202a0cbdabd1d8aaa730ed5ce5823590e9
Gregory <gregory.wyche@protonmail.com>
2019-06-14 12:33:59 -0400

    JUnit Test 3, not finished yet

---
+++ src/TTS/TeamBlue/Dealership/Utility/HelperTest.java
@@ -0,0 +1,14 @@
+package TTS.TeamBlue.Dealership.Utility;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelperTest {
+
+	@Test
+	void testQueueIndexTool() {
+		fail("Not yet implemented");
+	}
+
+}
